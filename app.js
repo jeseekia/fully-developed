7251ad1679a61b7f1a09fa71aaa9fe1e09ca69c4
Jeseekia Vaughn <jeseekia@gmail.com>
2017-03-14 16:53:48 -0400

    Setup working routes and corresponding views

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 var app = angular.module('mainApp',['ngRoute']);
 
+app.config(function($routeProvider, $locationProvider){
+
+  $routeProvider.when('/', {
+    controller: 'mainCtrl',
+    templateUrl: 'views/home.html'
+  }).when('/about', {
+    controller: 'mainCtrl',
+    templateUrl: 'views/about.html'
+  }).when('/portfolio', {
+    controller: 'mainCtrl',
+    templateUrl: 'views/portfolio.html'
+  }).when('/contact', {
+    controller: 'mainCtrl',
+    templateUrl: 'views/contact.html'
+  });
+
+  $locationProvider.hashPrefix('');
+});
+
 app.controller('mainCtrl', function($scope){
 
-  
+
 });
